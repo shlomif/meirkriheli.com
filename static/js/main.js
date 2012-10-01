7c2dcec690a6e352e6629b2572d5bcc24e3c7170
Meir Kriheli <mkriheli@gmail.com>
2012-10-01 21:05:07 +0200

    Toggle menu on button click

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,10 @@
+(function($) {
+	$(function() {
+		$('#main-menu-toggle').click(function(e) {
+			e.preventDefault();
+			$('#main-header nav').toggle();
 
+			return false;
+		});
+	})
+})(jQuery);
